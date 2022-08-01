ed271d9fa3a066c698e3ab9ff2b84d65c324b718
saptarshi chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-08-01 23:52:01 +0530

    added question 9(time adder)and its algo

--- Q9/Adder.java
+++ Q9/Adder.java
@@ -1,7 +1,26 @@
+/*
+ *Example: Time A 6 hours 35 minutes Time B Their sum is 7 hours 45 minutes 14 hours 20 minutes (where 60 minutes = 1 hour)
+
+ Class name: Adder
+ Data member/instance variable
+a[]
+Member functions/methods
+Adder()
+void readtime()
+void addtime(Adder X, Adder Y)
+integer array to hold two elements (hours and minutes)
+constructor to assign 0 to the array elements
+to enter the elements of the array
+adds the time of the two parameterized objects X and Y and stores the sum in the current calling object
+void disptime()
+displays the array elements with an appropriate message (i.e. hours = and minutes =
+Specify the class Adder giving details of the constructor(), void readtime(), void addtime (Adder, Adder) and void disptime(). Define the main() function to create objects and call the functions accordingly to enable the task.
+ */
+
 import java.util.Scanner;
 public class Adder{
-static int a[]=new int[2];
-static int h_ans=0,m_ans=0;
+ int a[]=new int[2];
+ int h_ans=0,m_ans=0;
 Adder(){
 a[0]=0;
 a[1]=0;
