3826da5c368c15f97b8f4da25c1ef296be93542d
saptarshi chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-09-04 22:52:07 +0530

    added code for binary to decimal and vice versa

---
+++ Q13/DecToBin.java
@@ -0,0 +1,31 @@
+/*Write a menu driven program in java to convert a binary to decimal and vice versa
+ */
+
+import java.util.Scanner;
+public class DecToBin{
+	int binary_to_decimal(int n){
+		String s=Integer.toString(n);
+		int ans=Integer.parseInt((Integer.toString(n)),2);
+		return ans;
+	}
+	int decimal_to_binary(int n){
+		String ans=Integer.toBinaryString(n);
+		return (Integer.parseInt(ans));
+	}
+	public static void main(String arg[]){
+		Scanner nrt=new Scanner(System.in);
+		System.out.println("1: Binary to Decimal\n2:Decimal to Binary");
+		int choice=nrt.nextInt();
+		System.out.println("Enter the number:");
+		int num=nrt.nextInt();
+		DecToBin A=new DecToBin();
+		if(choice==1){
+			System.out.println("Decimal version of "+num+" is "+(A.binary_to_decimal(num)));
+		}
+		else if (choice == 2){
+			System.out.println("Binary version of "+num+" is "+(A.decimal_to_binary(num)));	
+		}
+		nrt.close();
+	}
+
+}
